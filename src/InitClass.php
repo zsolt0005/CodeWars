b50baddf3feb73e4b9ad2532149dad9da89df520
Zsolt Döme <zsolt.dome@sportisimo.com>
2023-12-19 22:53:24 +0100

    Init

---
+++ src/InitClass.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+namespace Zsolt\CodeWars;
+
+/**
+ * Class InitClass
+ *
+ * @author  Zsolt Döme
+ * @since   2023
+ */
+final class InitClass
+{
+    public static function true(): bool
+    {
+        return true;
+    }
+}
